0b5c5f0ae7eff289b0a51c4e47491cc0127e3f05
Dungeon_Master <2012005asdfgt@gmail.com>
2024-10-25 22:02:15 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp7/MainForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AccessibleName = "";
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label1.Location = new System.Drawing.Point(41, 92);
+            this.Label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(53, 21);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Кому";
+            this.Label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AccessibleName = "";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(527, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(298, 23);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Отправление email писем";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AccessibleName = "";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AccessibleName = "";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(41, 170);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Заголовок";
+            // 
+            // label6
+            // 
+            this.label6.AccessibleName = "";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(41, 204);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Текст";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEmail.Location = new System.Drawing.Point(142, 135);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(271, 20);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBoxSubject.Location = new System.Drawing.Point(142, 172);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(632, 20);
+            this.textBoxSubject.TabIndex = 7;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(579, 133);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(195, 20);
+            this.textBoxName.TabIndex = 8;
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxBody.Location = new System.Drawing.Point(45, 228);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(729, 150);
+            this.textBoxBody.TabIndex = 9;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(302, 404);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(196, 34);
+            this.buttonSend.TabIndex = 10;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ WindowsFormsApp7/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using WindowsFormsApp7.UsersClasses;
+
+namespace WindowsFormsApp7
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxEmail.Text = "task_code_development@list.ru";
+            textBoxName.Text = "Антон Игоревич";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxEmail.Text) ||
+            string.IsNullOrWhiteSpace(textBoxName.Text) ||
+            string.IsNullOrWhiteSpace(textBoxSubject.Text) ||
+            string.IsNullOrWhiteSpace(textBoxBody.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            string smtp = "smtp.mail.ru";
+            StringPair fromInfo = new StringPair("GOIDA_2024@ZZ.ru", " СВященная война ");
+            string password = "imMiQNjW41DNajQ0QYbc";
+            StringPair toInfo = new StringPair(textBoxEmail.Text, textBoxName.Text);
+            string subject = textBoxSubject.Text;
+            string body = $"{DateTime.Now} \n" +
+            $"{Dns.GetHostName()}\n" +
+            $"{Dns.GetHostAddresses(Dns.GetHostName()).First()} \n" +
+            $"{textBoxBody.Text}";
+            InfoEmailSending info =
+                new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+            SendingEmail sendingEmail = new SendingEmail(info);
+            sendingEmail.Send();
+
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+
+        }
+    }
+}
